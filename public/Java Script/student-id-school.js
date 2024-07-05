571db2736e5e0d28caaab3ed545924feb10c91d0
okotochris <okoto.chris.oc@gmail.com>
2024-07-05 18:23:29 +0100

    file uploaded

--- public/Java Script/student-id-school.js
+++ public/Java Script/student-id-school.js
@@ -12,7 +12,7 @@ getId.onclick = async (e) => {
    
     try {
         console.log(student_name)
-        let response = await fetch(`https://www.myschoolresult.com/getstudentid?student_name=${student_name}`);
+        let response = await fetch(`http://localhost:3000/getstudentid?student_name=${student_name}`);
         if (!response.ok) {
             let div = document.createElement('div')
             let container = document.querySelector('.table')
