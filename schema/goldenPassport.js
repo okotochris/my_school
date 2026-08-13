7d0c9936604e22283cf1dc6f8f1f53c89a7770af
okoto.chris <okoto.chis.oc@gmail.com>
2026-08-13 14:47:09 +0100

    Complete subject update

---
+++ schema/goldenPassport.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const blogschema = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    studentId: {
+        type: String,
+        required: true
+    },
+    schoolsession: {
+        type: String,
+        required: true
+    },
+    addmissionNo: {
+        type: String
+    },
+    dob: {
+        type: String,
+    },
+    class:{
+        type: String,
+        required: true
+    },
+    passport: {
+        type: String,
+    },
+    gender:{
+        type:String
+    },
+    schoolName:{
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const Studentpassport = mongoose.model('Studentpassport', blogschema);
+module.exports = Studentpassport;
